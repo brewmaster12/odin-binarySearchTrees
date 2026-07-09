5f64df4898f1e4687c4eab50ca88c6e24d24702c
Sean <seanywg@gmail.com>
2026-07-09 14:20:27 +0100

    add buildTree() function

--- bst.js
+++ bst.js
@@ -3,10 +3,31 @@ function Node(data, leftChild, rightChild) {
 }
 
 function Tree(arr) {
+    // Sort and remove duplicates
+    arr = [...new Set(arr)].sort((a, b) => a - b);
+
+    function buildTree(arr) {
+        // Base case
+        if (arr.length === 1) return Node(arr[0]);
+        if (arr.length === 0) return; 
+
+        const start = 0;
+        const end = arr.length - 1;
+        const mid = Math.round((start + end) / 2);
+
+        // Split array in two
+        const leftArr = arr.slice(start, mid);
+        const rightArr = arr.slice(mid+1, arr.length);
+
+        // Recursive call for each subarray
+        const leftSubTree = buildTree(leftArr);
+        const rightSubTree = buildTree(rightArr);
+
+        const tree = Node(arr[mid], leftSubTree, rightSubTree);
+
+        return tree;
+    }
+
     const root = buildTree(arr);
     return { root }
-}
-
-function buildTree(arr) {
-
 }
