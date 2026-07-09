a9a3652c753d01132269cb953585e5aa11fe8876
Sean <seanywg@gmail.com>
2026-07-09 13:37:48 +0100

    add Tree() factory and buildTree() signature

--- bst.js
+++ bst.js
@@ -1,3 +1,12 @@
 function Node(data, leftChild, rightChild) {
     return { data, leftChild, rightChild }
 }
+
+function Tree(arr) {
+    const root = buildTree(arr);
+    return { root }
+}
+
+function buildTree(arr) {
+
+}
